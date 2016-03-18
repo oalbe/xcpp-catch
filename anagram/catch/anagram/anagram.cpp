ba6f4a5b5fcafdee90236b869796ebc46f5f7429
Alberto <alberto.ognibene@gmail.com>
2016-03-18 13:10:18 +0100

    Refactor anagram.h to not use 'is_permutation' anymore.
    This because 'is_permutation()' seem to run in O(n^2) while the current solution should run, at worst, in O(n*log(n)).

--- anagram/catch/anagram/anagram.cpp
+++ anagram/catch/anagram/anagram.cpp
@@ -1,23 +1,47 @@
 #include "anagram.h"
 
-std::string anagram::anagram::lower(const std::string& in_str) const {
-    std::string output_str = in_str;
+std::string anagram::anagram::lower(const std::string& input_string) const {
+    std::string output_str = input_string;
     std::transform(output_str.begin(), output_str.end(), output_str.begin(), ::tolower);
     
     return output_str;
+}
+
+std::map<char, std::size_t> anagram::anagram::map_word(std::string sequence) const {
+    std::map<char, std::size_t> mapped_sequence;
+    
+    std::size_t sequence_size = sequence.size();
+    for (std::size_t i = 0; i < sequence_size; ++i) {
+        if (mapped_sequence.count(sequence[i])) {
+            mapped_sequence[sequence[i]] += 1;
+        } else {
+            mapped_sequence[sequence[i]] = 1;
+        }
+    }
+    
+    return mapped_sequence;
+}
+
+bool anagram::anagram::is_anagram(std::string candidate) const {
+    if (sample_size != candidate.size()) {
+        return false;
+    }
+
+    return map_word(candidate) == mapped_sample;
 }
 
 std::vector<std::string>
 anagram::anagram::matches(const std::vector<std::string>& candidates) const {
     std::vector<std::string> __matches;
     
-    for (size_t i = 0; i < candidates.size(); ++i) {
+    std::size_t candidates_size = candidates.size();
+    for (size_t i = 0; i < candidates_size; ++i) {
         std::string current = lower(candidates[i]);
-        if (word == current) {
+        if (sample == current) {
             continue;
         }
-
-        if (std::is_permutation(current.begin(), current.end(), word.begin())) {
+        
+        if (is_anagram(current)) {
             __matches.push_back(candidates[i]);
         }
     }
--- anagram/catch/anagram/anagram.h
+++ anagram/catch/anagram/anagram.h
@@ -4,16 +4,27 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <map>
 
 
 namespace anagram {
     class anagram {
-        std::string word;
+        std::string sample;
+        std::size_t sample_size;
         
-        std::string lower(const std::string&) const;
+        std::map<char, std::size_t> mapped_sample;
+        std::size_t mapped_sample_size;
+        
+        std::string lower(const std::string& input_string) const;
+        std::map<char, std::size_t> map_word(std::string sequence) const;
     public:
-        anagram(const std::string input): word(lower(input)) {}
+        anagram(const std::string input): 
+            sample(lower(input)), 
+            sample_size(sample.size()),
+            mapped_sample(map_word(sample)),
+            mapped_sample_size(mapped_sample.size()) {}
 
+        bool is_anagram(std::string candidate) const;
         std::vector<std::string> matches(const std::vector<std::string>& candidates) const;
     };
 }
