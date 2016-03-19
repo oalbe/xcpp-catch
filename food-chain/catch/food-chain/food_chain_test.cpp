dd110d3bfe82bc3b28b52c9f6d45d878e9872769
Alberto <alberto.ognibene@gmail.com>
2016-03-19 14:34:42 +0100

    Create Catch tests and CMakeLists.txt for 'food-chain' exercism.
    Implementation missing.

---
+++ food-chain/catch/food-chain/food_chain_test.cpp
@@ -0,0 +1,112 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch/catch.hpp>
+
+#include "food_chain.h"
+
+
+TEST_CASE("Fly", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a fly.\n"
+        "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(1));
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+TEST_CASE("Spider", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a spider.\n"
+        "It wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(2));
+}
+
+TEST_CASE("Bird", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a bird.\n"
+        "How absurd to swallow a bird!\n"
+        "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(3));
+}
+
+TEST_CASE("Cat", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a cat.\n"
+        "Imagine that, to swallow a cat!\n"
+        "She swallowed the cat to catch the bird.\n"
+        "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. "
+        "Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(4));
+}
+
+TEST_CASE("Dog", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a dog.\n"
+        "What a hog, to swallow a dog!\n"
+        "She swallowed the dog to catch the cat.\n"
+        "She swallowed the cat to catch the bird.\n"
+        "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. "
+        "Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(5));
+}
+
+TEST_CASE("Goat", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a goat.\n"
+        "Just opened her throat and swallowed a goat!\n"
+        "She swallowed the goat to catch the dog.\n"
+        "She swallowed the dog to catch the cat.\n"
+        "She swallowed the cat to catch the bird.\n"
+        "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. "
+        "Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(6));
+}
+
+TEST_CASE("Cow", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a cow.\n"
+        "I don't know how she swallowed a cow!\n"
+        "She swallowed the cow to catch the goat.\n"
+        "She swallowed the goat to catch the dog.\n"
+        "She swallowed the dog to catch the cat.\n"
+        "She swallowed the cat to catch the bird.\n"
+        "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. "
+        "Perhaps she'll die.\n";
+
+    REQUIRE(expected == food_chain::verse(7));
+}
+
+TEST_CASE("Horse", "[answer]") {
+    std::string expected = "I know an old lady who swallowed a horse.\n"
+        "She's dead, of course!\n";
+
+    REQUIRE(expected == food_chain::verse(8));
+}
+
+TEST_CASE("Multiple_verses", "[answer]") {
+    std::string expected = 
+        "I know an old lady who swallowed a fly.\n"
+        "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+        "\n"
+        "I know an old lady who swallowed a spider.\n"
+        "It wriggled and jiggled and tickled inside her.\n"
+        "She swallowed the spider to catch the fly.\n"
+        "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+        "\n";
+
+    REQUIRE(expected == food_chain::verses(1, 2));
+}
+
+TEST_CASE("The_whole_song", "[answer]") {
+    REQUIRE(food_chain::verses(1, 8) == food_chain::sing());
+}
+#endif
