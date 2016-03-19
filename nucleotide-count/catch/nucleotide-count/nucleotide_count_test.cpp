2a3249e5214e8837cc0b38c3c79ddf7c8848fd10
Alberto <alberto.ognibene@gmail.com>
2016-03-19 14:48:14 +0100

    Create Catch tests and CMakeLists.txt for 'nucleotide-count' exercism.
    Implementation missing at the moment.

---
+++ nucleotide-count/catch/nucleotide-count/nucleotide_count_test.cpp
@@ -0,0 +1,67 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch/catch.hpp>
+#include <stdexcept>
+
+#include "nucleotide_count.h"
+
+
+TEST_CASE("Has no nucleotides", "[answer]") {
+    const dna::counter dna("");
+    const std::map<char, int> expected{ {'A', 0}, {'T', 0}, {'C', 0}, {'G', 0} };
+
+    const auto actual = dna.nucleotide_counts();
+
+    REQUIRE(expected == actual);
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+TEST_CASE("Has no adenosine", "[answer]") {
+    const dna::counter dna("");
+
+    REQUIRE(0 == dna.count('A'));
+}
+
+TEST_CASE("Repetitive cytidine gets counts", "[answer]") {
+    const dna::counter dna("CCCCC");
+
+    REQUIRE(5 == dna.count('C'));
+}
+
+TEST_CASE("Repetitive sequence has only guanosine", "[answer]") {
+    const dna::counter dna("GGGGGGGG");
+    const std::map<char, int> expected{ {'A', 0}, {'T', 0}, {'C', 0}, {'G', 8} };
+
+    const auto actual = dna.nucleotide_counts();
+
+    REQUIRE(expected == actual);
+}
+
+TEST_CASE("Counts only thymidine", "[answer]") {
+    const dna::counter dna("GGGGTAACCCGG");
+
+    REQUIRE(1 == dna.count('T'));
+}
+
+TEST_CASE("Counts a nucleotide only once", "[answer]") {
+    const dna::counter dna("GGTTGG");
+
+    dna.count('T');
+
+    REQUIRE(2 == dna.count('T'));
+}
+
+TEST_CASE("Validates nucleotides", "[answer]") {
+    const dna::counter dna("GGTTGG");
+
+    REQUIRE_THROWS_AS(dna.count('X'), std::invalid_argument);
+}
+
+TEST_CASE("Counts all nucleotides", "[answer]") {
+    const dna::counter dna("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
+    std::map<char, int> expected{ {'A', 20}, {'T', 21}, {'G', 17}, {'C', 12} };
+
+    auto actual = dna.nucleotide_counts();
+
+    REQUIRE(expected == actual);
+}
+#endif
