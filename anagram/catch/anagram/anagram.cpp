1ed50ea7270d5e944d7c052b460a21c71c09d716
Alberto <alberto.ognibene@gmail.com>
2016-03-20 18:37:24 +0100

    Optimize 'anagram' implementation.
    - Create an alias ('histogram_t') for the complex 'map' type.
    - Remove the need for 'zero_mapped_sample' and replace it with a 'for'.
    - Remove the 'if' inside the constructor loop because initialization is automatical in that case.
    - Wrapped the .cpp file inside the 'namespace anagram' instead of having to use the scope specifier for every method name'.

--- anagram/catch/anagram/anagram.cpp
+++ anagram/catch/anagram/anagram.cpp
@@ -1,36 +1,39 @@
 #include "anagram.h"
 
 
-anagram::anagram::anagram(const std::string input) {
+namespace anagram {
+
+anagram::anagram(const std::string& input) {
     sample = input;
     sample_size = sample.size();
     
     std::string lower_sample = lower(sample);
     for (char character : lower_sample) {
-        if (mapped_sample.count(character)) {
-            mapped_sample[character] += 1;
-        } else {
-            mapped_sample[character] = 1;
-            zero_mapped_sample[character] = 0;
-        }
+        ++mapped_sample[character];
     }
 }
 
-bool anagram::anagram::is_anagram(const std::string candidate) const {
+bool anagram::is_anagram(const std::string& candidate) const {
     if (sample_size != candidate.size()) {
         return false;
     }
     
-    std::map<char, std::size_t> mapped_sample_copy(mapped_sample);
+    histogram_t mapped_sample_copy(mapped_sample);
     for (char character : candidate) {
         mapped_sample_copy[character] -= 1;
     }
 
-    return zero_mapped_sample == mapped_sample_copy;
+    for (auto& pair : mapped_sample_copy) {
+        if (pair.second) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 std::vector<std::string>
-anagram::anagram::matches(const std::vector<std::string>& candidates) const {
+anagram::matches(const std::vector<std::string>& candidates) const {
     std::vector<std::string> __matches;
     
     std::string lower_sample = lower(sample);
@@ -49,9 +52,11 @@ anagram::anagram::matches(const std::vector<std::string>& candidates) const {
     return __matches;
 }
 
-std::string anagram::anagram::lower(const std::string& input_string) const {
+std::string anagram::lower(const std::string& input_string) const {
     std::string output_str = input_string;
     std::transform(output_str.begin(), output_str.end(), output_str.begin(), ::tolower);
     
     return output_str;
 }
+
+} // namespace anagram
--- anagram/catch/anagram/anagram.h
+++ anagram/catch/anagram/anagram.h
@@ -8,24 +8,25 @@
 
 
 namespace anagram {
-    class anagram {
-        // The word against which the program tests candidate anagrams.
-        std::string sample;
-        std::size_t sample_size;
-        
-        // A map representation of the `sample` with the frequency of each character.
-        std::map<char, std::size_t> mapped_sample;
-        
-        // A map representation of the `sample`, but with zeros instead of characters' frequencies.
-        std::map<char, std::size_t> zero_mapped_sample;
-        
-        std::string lower(const std::string& input_string) const;
-    public:
-        anagram(const std::string input);
 
-        bool is_anagram(const std::string candidate) const;
-        std::vector<std::string> matches(const std::vector<std::string>& candidates) const;
-    };
-}
+typedef std::map<char, std::size_t> histogram_t;
+
+class anagram {
+    // The word against which the program tests candidate anagrams.
+    std::string sample;
+    std::size_t sample_size;
+    
+    // A map representation of the `sample` with the frequency of each character.
+    histogram_t mapped_sample;
+    
+    std::string lower(const std::string& input_string) const;
+public:
+    anagram(const std::string& input);
+
+    bool is_anagram(const std::string& candidate) const;
+    std::vector<std::string> matches(const std::vector<std::string>& candidates) const;
+};
+
+} // namespace anagram
 
 #endif // ANAGRAM_H
