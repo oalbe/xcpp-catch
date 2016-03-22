34bb37ce51d86bd3ec50f5d8453fa2004d1c20d3
Alberto <alberto.ognibene@gmail.com>
2016-03-22 13:10:33 +0100

    Modify 'std::string to_rna()' to use the 'char' version of it.

--- rna-transcription/catch/rna-transcription/rna_transcription.h
+++ rna-transcription/catch/rna-transcription/rna_transcription.h
@@ -5,26 +5,27 @@
 
 
 namespace transcription {
-    std::map<char, char> dna_to_rna = {
-        {'G', 'C'},
-        {'C', 'G'},
-        {'T', 'A'},
-        {'A', 'U'}
-    };
-    
-    char to_rna(const char dna_nucleotide) {
-        return dna_to_rna[dna_nucleotide];
-    }
-    
-    std::string to_rna(const std::string& dna_strand) {
-        std::string rna;
-        for (const char nucleotide : dna_strand) {
-            rna += dna_to_rna[nucleotide];
-        }
-        
-        return rna;
-    }
+
+std::map<char, char> dna_to_rna = {
+    {'G', 'C'},
+    {'C', 'G'},
+    {'T', 'A'},
+    {'A', 'U'}
+};
+
+char to_rna(const char dna_nucleotide) {
+    return dna_to_rna[dna_nucleotide];
 }
 
+std::string to_rna(const std::string& dna_strand) {
+    std::string rna;
+    for (const char nucleotide : dna_strand) {
+        rna += to_rna(nucleotide);
+    }
+    
+    return rna;
+} // namespace transcription
+
+}
 
 #endif // RNA_TRANSCRIPTION_H
