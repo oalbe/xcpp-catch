8769687f3b1d3d83df3def1b1dad17553c7d29a8
Alberto <alberto.ognibene@gmail.com>
2016-03-23 17:17:18 +0100

    Add the last part of the testsuite for 'phone-number'.
    Now the testsuite works.

--- phone-number/catch/phone-number/phone_number_test.cpp
+++ phone-number/catch/phone-number/phone_number_test.cpp
@@ -3,46 +3,47 @@
 #include "phone_number.h"
 
 
-TEST_CASE("Cleans_parens_dashes_and_spaces_from_the_number", "[answer]") {
+TEST_CASE("Cleans parens dashes and spaces from the number", "[answer]") {
     const phone_number phone("(123) 456-7890");
 
-    REQUIRE("1234567890", phone.number());
+    REQUIRE("1234567890" == phone.number());
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-TEST_CASE("Cleans_numbers_with_dots", "[answer]") {
+TEST_CASE("Cleans numbers with dots", "[answer]") {
     const phone_number phone("123.456.7890");
 
-    REQUIRE("1234567890", phone.number());
+    REQUIRE("1234567890" == phone.number());
 }
 
-TEST_CASE("Valid_when_11_digits_and_first_digit_is_1", "[answer]") {
+TEST_CASE("Valid when 11 digits and first digit is 1", "[answer]") {
     const phone_number phone("11234567890");
 
-    REQUIRE("1234567890", phone.number());
+    REQUIRE("1234567890" == phone.number());
 }
 
-TEST_CASE("Invalid_when_11_digits", "[answer]") {
+TEST_CASE("Invalid when 11 digits", "[answer]") {
     const phone_number phone("21234567890");
 
-    REQUIRE("0000000000", phone.number());
+    REQUIRE("0000000000" == phone.number());
 }
 
-TEST_CASE("Invalid_when_9_digits", "[answer]") {
+TEST_CASE("Invalid when 9 digits", "[answer]") {
     const phone_number phone("123456789");
 
-    REQUIRE("0000000000", phone.number());
+    REQUIRE("0000000000" == phone.number());
 }
 
-TEST_CASE("Has_an_area_code", "[answer]") {
+TEST_CASE("Has an area code", "[answer]") {
     const phone_number phone("1234567890");
 
-    REQUIRE("123", phone.area_code());
+    REQUIRE("123" == phone.area_code());
 }
 
-TEST_CASE("Formats_a_number", "[answer]") {
+TEST_CASE("Formats a number", "[answer]") {
     const phone_number phone("1234567890");
 
-    REQUIRE("(123) 456-7890", std::string(phone));
+    REQUIRE("(123) 456-7890" == std::string(phone));
 }
-#endif
+
+// #if defined(EXERCISM_RUN_ALL_TESTS)
+// #endif
