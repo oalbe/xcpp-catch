4b7014870a3bb4277987c96f465f473021aa2e4b
Alberto <alberto.ognibene@gmail.com>
2016-03-29 13:20:12 +0200

    Create Catch tests and CMakeLists.txt for 'grains' exercism

---
+++ grains/grains_test.cpp
@@ -0,0 +1,39 @@
+#define CATCH_CONFIG_MAIN
+#include <catch/catch.hpp>
+#include "grains.h"
+
+
+TEST_CASE("Square 1", "[answer]") {
+    REQUIRE(1ULL == grains::square(1));
+}
+
+TEST_CASE("Square 2", "[answer]") {
+    REQUIRE(2ULL == grains::square(2));
+}
+
+TEST_CASE("Square 3", "[answer]") {
+    REQUIRE(4ULL == grains::square(3));
+}
+
+TEST_CASE("Square 4", "[answer]") {
+    REQUIRE(8ULL == grains::square(4));
+}
+
+TEST_CASE("Square 16", "[answer]") {
+    REQUIRE(32768ULL == grains::square(16));
+}
+
+TEST_CASE("Square 32", "[answer]") {
+    REQUIRE(2147483648ULL == grains::square(32));
+}
+
+TEST_CASE("Square 64", "[answer]") {
+    REQUIRE(9223372036854775808ULL == grains::square(64));
+}
+
+TEST_CASE("Total", "[answer]") {
+    REQUIRE(18446744073709551615ULL == grains::total());
+}
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
+#endif
