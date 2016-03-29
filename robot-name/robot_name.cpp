301dd8ae014cb4de8fceaec4e0d651860e777f6a
Alberto <alberto.ognibene@gmail.com>
2016-03-29 13:18:50 +0200

    Fix the broken generation of random numbers.
    - Fix 'generate_name()' 's 'random_device' by bringing it at namespace scope
    - Change ascii codes with actual chars ('A' and 'Z') in 'rand_ascii()'

--- robot-name/robot_name.cpp
+++ robot-name/robot_name.cpp
@@ -8,10 +8,11 @@ namespace robot_name {
     
 namespace {
 
+std::random_device rd;
+std::mt19937 gen(rd());
+
 std::string generate_name() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> rand_ascii(65, 90);
+    std::uniform_int_distribution<> rand_ascii('A', 'Z');
     std::uniform_int_distribution<> rand_digits(0, 999);
 
     char name_buffer[6]; // 5 chars for the name, 1 char for the null-termination
