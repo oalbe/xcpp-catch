b910afb79b4a1f02f695f1126f9b3af2f3e69c7d
Alberto <alberto.ognibene@gmail.com>
2016-04-07 12:32:26 +0200

    Split implementation in two files for 'binary' exercism

---
+++ binary/binary.cpp
@@ -0,0 +1,35 @@
+#include "binary.h"
+#include <cmath>
+
+
+namespace binary {
+
+namespace {
+
+bool validate_binary(std::string binary_string) {
+    for (std::size_t i = 0; i < binary_string.size(); ++i) {
+        if (('0' != binary_string[i]) && ('1' != binary_string[i])) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+} // unnamed namespace
+
+int convert(std::string binary_string) {
+    if (!validate_binary(binary_string)) {
+        return 0;
+    }
+    
+    int decimal = 0;
+
+    for (std::size_t i = binary_string.size(); i-- > 0; ) {
+        decimal += (binary_string[i] - '0') * (std::pow(2, (binary_string.size() - 1) - i));
+    }
+
+    return decimal;
+}
+
+} // namespace binary
