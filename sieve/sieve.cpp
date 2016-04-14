245ff6d311e00adfb6b24bca21856f6789e93775
Alberto <alberto.ognibene@gmail.com>
2016-04-14 17:01:43 +0200

    Change 'std::vector<bool>' to 'std::vector<char>'
    Reason: because 'std::vector<bool>' is different from all other kinds. It can be implemented as a vector of bits instead of bytes, and cause problems and slowness. (here: http://exercism.io/submissions/08d099cc873c4b22b72aeb19e772df6b and the reference for info)
    
    Also, uncommenting the second test generates a segmentation fault. Still figuring out why.

--- sieve/sieve.cpp
+++ sieve/sieve.cpp
@@ -7,20 +7,21 @@ namespace sieve {
 namespace {
 
 // Takes care of initializing the sieve with all `false` values
-std::vector<bool> initialize_sieve(std::size_t limit) {
-    std::vector<bool> empty_sequence(limit, false);
+std::vector<char> initialize_sieve(std::size_t limit) {
+    std::vector<char> empty_sequence(limit, 0);
     
-    empty_sequence[0] = true;
-    empty_sequence[1] = true;
+    empty_sequence[0] = 1;
+    empty_sequence[1] = 1;
 
     return empty_sequence;
 }
 
-sieve_t vector_convert_helper(std::vector<bool> sequence) {
+// REVIEW: Should the `vector` here be passed by const reference?
+sieve_t vector_convert_helper(std::vector<char> sequence) {
     sieve_t output;
     
     for (std::size_t i = 0; i < sequence.size(); ++i) {
-        if (false == sequence[i]) {
+        if (0 == sequence[i]) {
             output.push_back(i);
         }
     }
@@ -31,19 +32,22 @@ sieve_t vector_convert_helper(std::vector<bool> sequence) {
 } // unnamed namespace
 
 sieve_t primes(std::size_t limit) {
-    std::vector<bool> primes_sequence = initialize_sieve(limit);
+    std::vector<char> primes_sequence = initialize_sieve(limit);
     
+    // for (std::size_t i = 2; i <= std::sqrt(limit); ++i) {
     for (std::size_t i = 2; i <= std::sqrt(limit); ++i) {
-        if (true == primes_sequence[i]) {
+        if (1 == primes_sequence[i]) {
             continue;
         }
         
         for (std::size_t j = i * i; j <= limit; j += i) {
-            primes_sequence[j] = true;
+            primes_sequence[j] = 1;
         }
     }
+
+    sieve_t converted_vector = vector_convert_helper(primes_sequence);
     
-    return vector_convert_helper(primes_sequence);
+    return converted_vector;
 }
 
 } // namespace sieve
