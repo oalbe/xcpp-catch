852dc72badee82eaccbf3f8a015189edd8c48ca5
Alberto <alberto.ognibene@gmail.com>
2016-04-20 12:13:01 +0200

    Split implementation in two files for 'queen-attack' exercism

---
+++ queen-attack/queen_attack.cpp
@@ -0,0 +1,57 @@
+#include "queen_attack.h"
+#include <stdexcept>
+
+
+namespace queen_attack {
+
+chess_board::chess_board():
+    __white(std::make_pair(0, 3)), __black(std::make_pair(7, 3)) {}
+
+chess_board::chess_board(square_t white_queen, square_t black_queen) {
+    if (white_queen == black_queen) {
+        throw std::domain_error("You can't place both queens on the same square.");
+    }
+    
+    __white = white_queen;
+    __black = black_queen;
+}
+
+square_t chess_board::white() const { return __white; }
+square_t chess_board::black() const { return __black; }
+
+chess_board::operator std::string() const {
+    std::string stringified_board;
+    for (std::size_t i = 0; i < 8; ++i) {
+        stringified_board += "_ _ _ _ _ _ _ _\n";
+    }
+    
+    // REVIEW: Chose which one's best to use.
+    // std::size_t white_position = ((white().first + 1) * 16) - (16 - (white().second * 2));
+    // std::size_t white_position = (white().first * 16) + (white().second * 2);
+    std::size_t white_position = 2 * ((white().first * 8) + white().second);
+    std::size_t black_position = ((black().first + 1) * 16) - (16 - (black().second * 2));
+    
+    stringified_board[white_position] = 'W';
+    stringified_board[black_position] = 'B';
+    
+    return stringified_board;
+}
+
+bool chess_board::can_attack() const {
+    if (white().first == black().first) {
+        return true;
+    }
+    
+    if (white().second == black().second) {
+        return true;
+    }
+    
+    // Check if they are in the same diagonal
+    std::size_t deltax = white().first - black().first;
+    std::size_t deltay = white().second - black().second;
+
+    // REVIEW: This can be split into two `if`s, but is it worth it?
+    return (deltay == deltax) || (deltay == -deltax);
+}
+
+} // namespace queen_attack
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -1,68 +1,25 @@
 #ifndef QUEEN_ATTACK_H
 #define QUEEN_ATTACK_H
 #include <utility>
-#include <stdexcept>
 #include <string>
 
 
 namespace queen_attack {
 
+// REVIEW: Should this alias be defined inside the class? Can that actually be done?
 using square_t = std::pair<int, int>;
 
 class chess_board {
     square_t __white;
     square_t __black;
 public:
-    chess_board(): __white(std::make_pair(0, 3)), __black(std::make_pair(7, 3)) {}
-    chess_board(square_t white_queen, square_t black_queen) {
-        if (white_queen == black_queen) {
-            throw std::domain_error("You can't place both queens on the same square.");
-        }
-        __white = white_queen;
-        __black = black_queen;
-    }
+    chess_board();
+    chess_board(square_t white_queen, square_t black_queen);
     
-    square_t white() const {
-        return __white;
-    }
-    
-    square_t black() const {
-        return __black;
-    }
-    
-    operator std::string() const {
-        std::string stringified_board;
-        for (std::size_t i = 0; i < 8; ++i) {
-            stringified_board += "_ _ _ _ _ _ _ _\n";
-        }
-        
-        // REVIEW: Chose which one's best to use.
-        // std::size_t white_position = ((white().first + 1) * 16) - (16 - (white().second * 2));
-        // std::size_t white_position = (white().first * 16) + (white().second * 2);
-        std::size_t white_position = 2 * ((white().first * 8) + white().second);
-        std::size_t black_position = ((black().first + 1) * 16) - (16 - (black().second * 2));
-        
-        stringified_board[white_position] = 'W';
-        stringified_board[black_position] = 'B';
-        
-        return stringified_board;
-    }
-    
-    bool can_attack() const {
-        if (white().first == black().first) {
-            return true;
-        }
-        
-        if (white().second == black().second) {
-            return true;
-        }
-        
-        // Check if they are in the same diagonal
-        std::size_t deltax = white().first - black().first;
-        std::size_t deltay = white().second - black().second;
-
-        return (deltay == deltax) || (deltay == -deltax);
-    }
+    square_t white() const;
+    square_t black() const;
+    bool can_attack() const;
+    operator std::string() const;
 };
 
 } // namespace queen_attack
